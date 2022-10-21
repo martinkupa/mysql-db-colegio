f64af58a53412731f170ff82329c00a6da3ef54b
nehuenlaje <86781116+nehuenlaje@users.noreply.github.com>
2022-10-21 10:00:55 -0300

    Add files via upload
    si

--- insert_laje.sql
+++ insert_laje.sql
@@ -5,3 +5,5 @@
     CursoOptativo
     Responsable
 */
+INSERT INTO profesor VALUES ('00110011', 'Elmer', 'Galaga', '133713371337', '00022', '765 Sancho Pac-Man', 'galaga@namco.com', '1981-9-20')
+INSERT INTO profesor VALUES ('11001100', 'María', 'Mappy', '12341234123', '00011', '1122 Tito Faez', 'mappy@colegioreal.gov.ar', '1983-5-15')
