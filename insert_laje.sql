916458f573b6168230e9dd3ddf55d9e861c695a3
nehuenlaje <86781116+nehuenlaje@users.noreply.github.com>
2022-10-24 11:33:06 -0300

    Update insert_laje.sql

--- insert_laje.sql
+++ insert_laje.sql
@@ -8,4 +8,5 @@
 INSERT INTO profesor VALUES ('00110011', 'Elmer', 'Galaga', '133713371337', '00022', '765 Sancho Pac-Man', 'galaga@namco.com', '1981-9-20');
 INSERT INTO profesor VALUES ('11001100', 'María', 'Mappy', '12341234123', '00011', '1122 Tito Faez', 'mappy@colegioreal.gov.ar', '1983-5-15')
 INSERT INTO profesor VALUES ('00000001', 'Martín', 'Kupa', '432143214321', '10000', '0001 Obelisco', 'kupa@colegioreal.gov.ar', '0001-1-01')
-INSERT INTO profesor VALUES ('00000002', 'Joaquín', 'Da Silva', '12341234123', '00011', '1122 Tito Faez', 'mappy@colegioreal.gov.ar', '1983-5-15')
+INSERT INTO profesor VALUES ('00000002', 'Joaquín', 'Da Silva', '3333333333', '00002', '0002 Av Siempreviva', 'dasilva@colegiofalso.gov.ar', '2004-4-2')
+INSERT INTO profesor VALUES ('00000003', 'Nehuen', 'Laje', '222222222222', '00003', '0004 Carlos Garío', 'laje@colegiofalso.gov.ar', '2004-11-7')
