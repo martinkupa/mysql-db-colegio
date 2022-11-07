e45e721f2cdfc6da5554ff13558b32a0241afe7e
nehuenlaje <86781116+nehuenlaje@users.noreply.github.com>
2022-11-07 11:32:16 -0300

    Add files via upload

--- insert_laje.sql
+++ insert_laje.sql
@@ -11,9 +11,18 @@ INSERT INTO profesor VALUES ('00000001', 'Martín', 'Kupa', '432143214321', '100
 INSERT INTO profesor VALUES ('00000002', 'Joaquín', 'Da Silva', '3333333333', '00002', '0002 Av Siempreviva', 'dasilva@colegiofalso.gov.ar', '2004-4-2');
 INSERT INTO profesor VALUES ('00000003', 'Nehuen', 'Laje', '222222222222', '00003', '0004 Carlos Garío', 'laje@colegiofalso.gov.ar', '2004-11-7');
 INSERT INTO profesor VALUES ('00000004', 'Takumi', 'Fujiwara', '11111111111', '00086', '1234 Mt. Akina', 'takumi@speedstars.gov.ar', '1984-10-2');
-INSERT INTO profesor VALUES ('50000005', 'Homer', 'Simpson', '232232222322', '00005', '0784 Avenida Siempreviva', 'homero@plantanuclear.gov.ar', '1968-1-20');
+INSERT INTO profesor VALUES ('50000005', 'Homero', 'Simpson', '232232222322', '00005', '0784 Avenida Siempreviva', 'homero@plantanuclear.gov.ar', '1968-1-20');
 INSERT INTO profesor VALUES ('00000008', 'Mario', 'Mario', '322262222223', '10302', '1921 Calle Champiñón', 'mario@reinochmp.gov.ar', '1983-9-4');
 INSERT INTO profesor VALUES ('00000009', 'Simón', 'Nombrefalso', '123456789101', '12345', '4321 Calle Nombrecreativo', 'simon@mequedesinideas.gov.ar', '2000-1-1');
 INSERT INTO profesor VALUES ('00000010', 'Carlos', 'Carlíos', '022222222222', '00009', '6789 Carlos Carío', 'carlos@colegioficticio.gov.ar', '1999-12-30');
 
-INSERT INTO taller VALUES ('Soldadura', 'Elmer Galaga');
+INSERT INTO taller VALUES ('Soldadura', 'Elmer_Galaga');
+INSERT INTO taller VALUES ('Fundición', 'María_Mappy');
+INSERT INTO taller VALUES ('Electrónica', 'Martín_Kupa');
+INSERT INTO taller VALUES ('Carpentería', 'Joaquín_Da_Silva');
+INSERT INTO taller VALUES ('Automotores 1', 'Nehuen_Laje');
+INSERT INTO taller VALUES ('Automotores 2', 'Takumi_Fujiwara');
+INSERT INTO taller VALUES ('Automotores 3', 'Homero_Simpson');
+INSERT INTO taller VALUES ('Computación 1', 'Mario_Mario');
+INSERT INTO taller VALUES ('Computación 2', 'Simón_Nombrefalso');
+INSERT INTO taller VALUES ('Computación 3', 'Carlos_Carlíos');
