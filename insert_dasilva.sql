e74af7bd83a4b2b85ba0ebb25ad0ad6679202a62
da-silva-Joaco <88797924+da-silva-Joaco@users.noreply.github.com>
2022-11-07 08:57:30 -0300

    Update insert_dasilva.sql

--- insert_dasilva.sql
+++ insert_dasilva.sql
@@ -6,13 +6,16 @@
     Alumno
 */
 Curso:
-
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000003','4','3')
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000002','5','1')
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','2','5')
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('11001100','1','7')
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00110011','6','1')
-INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','4','1')
+INSERT INTO materias VALUES ('Lengua y Literatura');
+INSERT INTO materias VALUES ('Matematica');
+INSERT INTO materias VALUES ('Fisica');
+INSERT INTO materias VALUES ('Educacion Fisica');
+INSERT INTO materias VALUES ('Historia');
+INSERT INTO materias VALUES ('Economia');
+INSERT INTO materias VALUES ('Base de datos');
+INSERT INTO materias VALUES ('Dibujo Tecnico');
+INSERT INTO materias VALUES ('Redes');
+INSERT INTO materias VALUES ('Algoritmos');
 
 RotacionTaller:
 INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000003','4','3')
@@ -21,3 +24,10 @@ INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','2','5'
 INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('11001100','1','7')
 INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00110011','6','1')
 INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','4','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('50000005','3','5')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000004','2','7')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000010','1','2')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000009','2','3')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000008','6','2')
+
+
