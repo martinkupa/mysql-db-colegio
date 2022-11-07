d3989d9dcd0e851b461f35979129ac2dd8a37c1e
da-silva-Joaco <88797924+da-silva-Joaco@users.noreply.github.com>
2022-11-07 08:50:31 -0300

    Update insert_dasilva.sql

--- insert_dasilva.sql
+++ insert_dasilva.sql
@@ -5,3 +5,19 @@
     RotacionTaller
     Alumno
 */
+Curso:
+
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000003','4','3')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000002','5','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','2','5')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('11001100','1','7')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00110011','6','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','4','1')
+
+RotacionTaller:
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000003','4','3')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000002','5','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','2','5')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('11001100','1','7')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00110011','6','1')
+INSERT INTO rotaciontaller(FK_Profesor,año,rotacion) VALUES ('00000001','4','1')
